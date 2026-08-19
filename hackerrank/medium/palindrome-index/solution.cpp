b4793161fb5913c31e5de5cdf097d5ab65a8b265
Amina Kashif <ak.aminakashif@gmail.com>
2026-08-19 21:25:51 +0500

    ✅ Palindrome Index | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ hackerrank/medium/palindrome-index/solution.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int palindromeIndex(string s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+
+            // Try removing the left character
+            bool leftPalindrome = true;
+            int i = left + 1;
+            int j = right;
+
+            while (i < j) {
+                if (s[i] != s[j]) {
+                    leftPalindrome = false;
+                    break;
+                }
+                i++;
+                j--;
+            }
+
+            if (leftPalindrome) {
+                return left;
+            }
+
+            // Otherwise remove the right character
+            bool rightPalindrome = true;
+            i = left;
+            j = right - 1;
+
+            while (i < j) {
+                if (s[i] != s[j]) {
+                    rightPalindrome = false;
+                    break;
+                }
+                i++;
+                j--;
+            }
+
+            if (rightPalindrome) {
+                return right;
+            }
+
+            return -1;
+        }
+
+        left++;
+        right--;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = palindromeIndex(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
